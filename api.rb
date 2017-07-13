8f666bf1ec159be4d00d5e2e76f1c64e82419630
Anatoly Chernow <chertoly@gmail.com>
2017-07-13 22:40:55 +0300

    Prepare DB for development

--- api.rb
+++ api.rb
@@ -1,15 +1,20 @@
+require 'timeout'
+require 'sequel'
 require 'mysql2'
-require 'hobby'
-require 'hobby/json'
 
+DB_USER = ENV['DB_USER']
+DB_PASS = ENV['DB_PASS']
+DB_HOST = ENV['DB_HOST']
+DB_NAME = ENV['DB_NAME']
 
 # Throw a timeout error if the connection to the database
 # will not be established in 20 seconds.
-require 'timeout'
 DB = Timeout.timeout 20 do
   begin
-    Mysql2::Client.new host: 'db', username: 'root', password: 'root'
+    Mysql2::Client.new host: DB_HOST, username: DB_USER, password: DB_PASS
+    Sequel.connect "mysql2://#{DB_USER}:#{DB_PASS}@#{DB_HOST}/#{DB_NAME}"
   rescue Mysql2::Error
+    puts $!
     puts 'Waiting for db...'
     sleep 1
     retry
@@ -17,12 +22,15 @@ DB = Timeout.timeout 20 do
 end
 
 
+require 'hobby'
+require 'hobby/json'
+
 class API
   include Hobby
   include JSON
 
   get do
-    DB.query('show databases').to_a
+    DB.tables
   end
 
   get '/some_route' do
