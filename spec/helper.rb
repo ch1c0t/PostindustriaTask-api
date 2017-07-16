f5a23a53ba239ca54f97c806ab1639eda17d2ac4
Anatoly Chernow <chertoly@gmail.com>
2017-07-16 22:08:42 +0300

    Do clear DB after the test suite

--- spec/helper.rb
+++ spec/helper.rb
@@ -6,10 +6,18 @@ ENV['DB_HOST'] = '127.0.0.1'
 require_relative '../api'
 require 'hobby/devtools/rspec_helper'
 
+def clear_db
+  DB.run 'SET FOREIGN_KEY_CHECKS = 0'
+  DB[:companies].truncate
+  DB.run 'SET FOREIGN_KEY_CHECKS = 1'
+end
+
 RSpec.configure do |config|
   config.before :each do
-    DB.run 'SET FOREIGN_KEY_CHECKS = 0'
-    DB[:companies].truncate
-    DB.run 'SET FOREIGN_KEY_CHECKS = 1'
+    clear_db
+  end
+
+  config.after :suite do
+    clear_db
   end
 end
