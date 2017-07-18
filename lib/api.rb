4132bfab2a5ee29be1f6252c7f903b1f33e67bd9
Anatoly Chernow <chertoly@gmail.com>
2017-07-18 22:29:53 +0300

    A basic validation of a company name

--- lib/api.rb
+++ lib/api.rb
@@ -8,16 +8,33 @@ class Companies
   include JSON
 
   post do
-    name, quota = json.values_at 'name', 'quota'
-    hash = { name: name, quota: quota }
-    DB[:companies].insert hash
+    errors = validate json
 
-    response.status = 201
-    DB[:companies].where(name: name).first
+    if errors.empty?
+      name, quota = json.values_at 'name', 'quota'
+      hash = { name: name, quota: quota }
+      DB[:companies].insert hash
+
+      response.status = 201
+      DB[:companies].where(name: name).first
+    else
+      response.status = 422
+      { 'errors' => errors }
+    end
   end
 
   get do
     DB[:companies].all
+  end
+
+  def validate json
+    errors = {}
+
+    if json['name'].size > 255
+      errors['name'] = [ "cannot exceed 255 characters" ]
+    end
+
+    errors
   end
 end
 
